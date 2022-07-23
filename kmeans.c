28e8f8d6c7b08b60ed2dbfb1863dd61af0459fbd
yarindiga <100430301+yarindiga@users.noreply.github.com>
2022-07-23 06:05:09 +0300

    Update kmeans.c

--- kmeans.c
+++ kmeans.c
@@ -3,7 +3,6 @@
 #include <math.h>
  
 #define MAX_ITER(argc, argv) argc == 4 ? 200 : argv[2]
-// #define EPSILON 0.001
 #define ERROR_OCCURED "An Error Has Occured"
 #define INVALID_INPUT "Invalid Input!"
 
@@ -11,29 +10,6 @@ static void printerr(char *msg){
     printf("%s\n", msg);
     exit(1);
 }
-
-// static int count_rows(FILE *fp) 
-// {
-//     int ch;
-//     int lines = 0;
-
-//     while (EOF != (ch=getc(fp)))
-//         if (ch=='\n')
-//             ++lines;
-//     rewind(fp);
-//     return lines;
-// }
-
-// static int count_cols(FILE *fp)
-// {
-//     int cols = 0;
-//     do {
-//         fscanf(fp, "%*f");
-//         cols++;
-//     } while(getc(fp) != '\n');
-//     rewind(fp);
-//     return cols;
-// }
 
 static void free_double_pointer(double **dp, int k)
 {
@@ -51,28 +27,6 @@ static void free_data(double **points, double **centroids, double **clusters_sum
     free(clusters_counts);
 }
 
-
-// static int safe_strtol(char *arg)
-// {
-//     char *endptr;
-//     int n = strtol(arg, &endptr, 10);
-//     if (strlen(arg) ^ (endptr - arg) || n <= 0)
-//         printerr(INVALID_INPUT);
-    
-//     return n;
-// }
-
-// void process_arglist(int argc, char **argv, int *k, int *max_iter, char **input, char **output)
-// {
-//     int i = 0;
-//     if (argc != 5 && argc != 4) printerr(INVALID_INPUT);
-//     *k = safe_strtol(argv[++i]);
-
-//     if (argc == 5) *max_iter = safe_strtol(argv[++i]);
-//     else *max_iter = 200;
-//     *input = argv[++i];
-//     *output = argv[++i];
-// }
 
 static int update_centroids(double **centroids, double **clusters_sums, int *clusters_count, int k, int v_len, double epsilon)
 {
@@ -105,25 +59,6 @@ static int update_centroids(double **centroids, double **clusters_sums, int *clu
     return change_less_than_epsilon;
 }
 
-
-// double *read_matrix_line(FILE *fp, int v_len)
-// {
-//     int i;
-//     double *d_arr;
-
-//     d_arr = (double *)calloc(v_len,  sizeof(double));
-//     if (d_arr == NULL)
-//         printerr(ERROR_OCCURED);
-
-//     for (i = 0; i < v_len; i++) {
-//         fscanf(fp, "%lf", d_arr + i);
-//       /*  printf("%f ", d_arr[i]); */
-//         getc(fp);
-//     }
-    
-//     return d_arr;
-// }
-
 static double distance(double *p1, double *p2, int len)
 {
     double d = 0;
@@ -151,43 +86,6 @@ static int closest_cluster(double *point, int v_len, double ** centroids, int k)
 }
 
 
-// double ** read_input(char *filename, int *v_len, int *points_count)
-// {
-//     FILE *fp;
-//     int i, rows, cols;
-//     double **points;
-
-//     fp = fopen(filename, "r");
-//     if (fp == NULL) printerr(ERROR_OCCURED);
-    
-//     rows = count_rows(fp);
-//     cols = count_cols(fp);
-//     *points_count = rows;
-//     points = (double **)calloc(cols * *points_count, sizeof (double));
-//     if (points == NULL)
-//         printerr(ERROR_OCCURED);
-//     for (i = 0; i < *points_count; i++) points[i] = read_matrix_line(fp, cols);
-//     *v_len = cols;
-//     if (fclose(fp))
-//         printerr(ERROR_OCCURED);
-//     return points;
-// }
-
-// void write_output(char *filename, double **centroids, int k, int v_len)
-// {
-//     int i, j;
-//     FILE *fp = fopen(filename, "w");
-    
-//     for (i = 0; i < k; i++) {
-//         for (j = 0; j < v_len; j++) {
-//             fprintf(fp, "%.4f", centroids[i][j]);
-//             if (j == v_len - 1) putc('\n', fp);
-//             else putc(',', fp);
-//         }
-//     }
-//     if(fclose(fp))
-//         printerr(ERROR_OCCURED);
-// }
 static double **initialize_matrix(int n_rows, int n_cols)
 {
     double **matrix;
@@ -218,8 +116,6 @@ static double **matrix_py_to_native(PyObject *py_matrix)
     if (n_rows) n_cols = PyList_Size(PyList_GetItem(py_matrix, 0));
 
     matrix = initialize_matrix(n_rows, n_cols);
-    // printf("INITIALIZED MATRIX\n");
-    // print_nested(matrix, n_rows, n_cols);
     for (i = 0; i < n_rows; i++) {
         list = PyList_GetItem(py_matrix, i);
         for (j = 0; j < n_cols; j++) {
@@ -232,26 +128,9 @@ static double **matrix_py_to_native(PyObject *py_matrix)
                 return NULL;
             }
         }
-        // printf("matrix[%d][%d] = %f\n", i, 0, matrix[i][0]);
     }
-    // printf("matrix[%d][%d] = %f\n", 0, 0, matrix[0][0]);
-    // printf("IN FUNCTION\n");
-    // print_nested(matrix, n_rows, n_cols);
     return matrix;
 }
-
-// static void print_nested(double **m, int n_rows, int n_cols)
-// {
-//     int i, j;
-
-//     for (i = 0; i < n_rows; i++) {
-//         for (j = 0; j < n_cols; j++) {
-//             printf("%f,", m[i][j]);
-//         }
-//         putchar('\n');
-//     }
-// }
-
 
 static PyObject *fit_api(PyObject *self, PyObject *args)
 {
@@ -265,83 +144,20 @@ static PyObject *fit_api(PyObject *self, PyObject *args)
     int *clusters_counts;
     int i, j, counter = 0;
     PyObject *pylist, *pycent, *py_initial_centroids, *py_points;
-    // printf("HELLO EVERYONE!\n");
     if (!PyArg_ParseTuple(args, "OOiid", &py_initial_centroids, &py_points, &k, &max_iter, &epsilon))
         return NULL;
-    // printf("k = %d, max_iter = %d\n", k, max_iter);
-    // printf("HI THERE\n");
     if (!PyList_Check(py_initial_centroids) || !PyList_Check(py_points))
         return NULL;
     points_count = PyList_Size(py_points);
     if (points_count) v_len = PyList_Size(PyList_GetItem(py_points, 0));
-
-    // for (i = 0; i < n; i++) {
-    //     item = PyList_GetItem(_list, i);
-    //     if (!PyList_Check(item)){  /* We only print lists */
-    //        continue;
-    //     }
-    // }
-    
-    // centroids = (double **)calloc(k, sizeof(double *));
-    // if (centroids == NULL)
-    //     printerr(ERROR_OCCURED);
+ 
     clusters_counts = (int *)calloc(k, sizeof(int));
     if (clusters_counts == NULL) {
-        // printf("ON LINE 271\n");
         printerr(ERROR_OCCURED);
     }
-    // printf("ON LINE 274\n");
     clusters_sums = initialize_matrix(k, v_len);
-    // points = read_input(input_file, &v_len, &points_count);
-
-    // copying the py centorids into a native c matrix
     centroids = matrix_py_to_native(py_initial_centroids);
-    // printf("INITIALIZED CENTROIDS\n");
-    // print_nested(centroids, k, v_len);
     points = matrix_py_to_native(py_points);
-    // printf("INITIALIZED POINTS\n");
-    // print_nested(points, points_count, v_len);
-    // for (i = 0; i < k; i++) {
-    //     centroids[i] = (double *) calloc(v_len, sizeof(double));
-    //     if (centroids[i] == NULL)
-    //         printerr(ERROR_OCCURED);
-    //     clusters_sums[i] = (double *) calloc(v_len, sizeof(double));
-    //     if (clusters_sums[i] == NULL)
-    //         printerr(ERROR_OCCURED);
-    // }
-
-    // copying the py points into a native c matrix
-    // points_count = PyList_Size(py_points);
-    // for (i = 0; i < points_count; i++) {
-    //     points[i] = (double *)calloc(v_len, sizeof(double));
-    //     if (centroids[i] == NULL)
-    //         printerr(ERROR_OCCURED);
-    //     clusters_sums[i] = (double *)calloc(v_len, sizeof(double));
-    //     if (clusters_sums[i] == NULL)
-    //         printerr(ERROR_OCCURED);
-    // }
-
-    // for (i = 0; i < k; i++) {
-    //     centroids[i] = (double *)calloc(v_len, sizeof(double));
-    //     pycent = PyList_GetItem(py_initial_centroids, i);
-    //     for (j = 0; j < v_len; j++) {
-    //         centroids[i][j] = PyFloat_AsDouble(PyList_GetItem(pycent, i));
-    //         if (centroids[i][j]  == -1.0 && PyErr_Occurred()) {
-    //             free_data(centroids, clusters_sums, clusters_counts, k);
-    //             fprintf(stderr, ERROR_OCCURED);
-    //             return NULL;
-    //         }
-    //     }
-    //     if (centroids[i] == NULL)
-    //         printerr(ERROR_OCCURED);
-    //     clusters_sums[i] = (double *)calloc(v_len, sizeof(double));
-    //     if (clusters_sums[i] == NULL)
-    //         printerr(ERROR_OCCURED);
-    // }
-    // for (i = 0; i < k; i++) 
-    //     for (j = 0; j < v_len; j++) 
-    //         centroids[i][j] = points[i][j];
-    // print_nested(centroids, k, v_len);
     for (counter = 0; counter < max_iter; counter++) {
         for (i = 0; i < points_count; i++) {
             cluster_index = closest_cluster(points[i], v_len, centroids, k);
@@ -351,12 +167,10 @@ static PyObject *fit_api(PyObject *self, PyObject *args)
             }            
         }
         
-        // print_nested(centroids, k, v_len);
         is_converged = update_centroids(centroids, clusters_sums, clusters_counts, k, v_len, epsilon);
         if (is_converged)
             break;
     }
-    // write_output(output_file, centroids, k, v_len);
     pylist = PyList_New(k);
     for (i = 0; i < k; i++) {
         pycent = PyList_New(v_len);
@@ -364,8 +178,6 @@ static PyObject *fit_api(PyObject *self, PyObject *args)
             PyList_SetItem(pycent, j, Py_BuildValue("d", centroids[i][j]));
         PyList_SetItem(pylist, i, pycent);
     }
-    // printf("CENTROIDS:\n");
-    // print_nested(centroids, k, v_len);
     free_data(points, centroids, clusters_sums, clusters_counts, k, points_count);
     return pylist;
 }
@@ -394,8 +206,3 @@ PyMODINIT_FUNC PyInit_mykmeanssp(void)
     m = PyModule_Create(&moduledef);
     return m;
 }
-
-
-
-
-
